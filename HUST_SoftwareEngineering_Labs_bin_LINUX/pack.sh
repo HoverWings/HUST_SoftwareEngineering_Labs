fc98d7b4e024f30e827c07546dac94668425b28d
Hover <hover@hust.edu.cn>
2018-11-17 23:17:05 +0800

    Linux Finish

--- HUST_SoftwareEngineering_Labs_bin_LINUX/pack.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh  
-exe="FlightManagementSystem" #你需要发布的程序名称
-des="/home/hover/Desktop/Labs/HUST_SoftwareEngineering_Labs/build-FlightManagementSystem-Desktop-Release" #步骤1中的目录即本文件所在目录
-deplist=$(ldd $exe | awk  '{if (match($3,"/")){ printf("%s "),$3 } }')  
-cp $deplist $des
